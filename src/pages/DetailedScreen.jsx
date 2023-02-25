323856e47674839d7a67b61585d2bbc7480b340c
Tornike1123 <toko36106@gmail.com>
2023-02-25 21:22:31 +0400

    final vers

--- src/pages/DetailedScreen.jsx
+++ src/pages/DetailedScreen.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+import classes from "./DetailedScreen.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const DetailedScreen = () => {
 	const [productList, setProductList] = useState([]);
@@ -14,41 +18,70 @@ const DetailedScreen = () => {
 			});
 	}, []);
 
-	const allProducts = productList.map((product, inx) => {
-		const filtered = product.products.map((drink) => drink);
-		const filteredProducts = filtered.map((single) => single);
-		const ready = filteredProducts.filter((el) => el.id === +id);
-		const fetched = ready[0];
-		// const ddd = ready.specifications.map((dddd) => console.log(dddd));
-		//console.log(fetched);
-		const codeMy = fetched?.description;
-
-		return (
-			<div key={inx}>
-				<p>{fetched?.price}</p>
-
-				<p>{fetched?.name}</p>
-				<img
-					style={{ background: fetched?.bgColor }}
-					src={fetched?.mainPhoto}
-					alt=""
-				/>
-				<p>{fetched?.title}</p>
-				<p dangerouslySetInnerHTML={{ __html: codeMy }}></p>
-			</div>
-		);
-	});
-
 	const navigate = useNavigate();
 
 	const navigateHandler = () => {
 		navigate("/");
 	};
 
+	const allProducts = productList.map((product, inx) => {
+		const filtered = product.products.map((drink) => drink);
+		const filteredProducts = filtered.map((single) => single);
+		const ready = filteredProducts.filter((el) => el.id === +id);
+		const fetched = ready[0];
+		const specifications = fetched?.specifications.map((spec) =>
+			console.log(spec)
+		);
+		// const imgUrl = fetched?.imgUrls.map((imgUr) => console.log(imgUr));
+		console.log(fetched);
+
+		const codeMy = fetched?.description;
+
+		return (
+			<div className={classes.mainDiv} key={inx}>
+				<div className={classes.flex}>
+					<div>
+						<p className={classes.sz}>{fetched?.name}</p>
+						<p className={classes.sz2}>{fetched?.price}</p>
+					</div>
+					<div
+						style={{ background: fetched?.bgColor }}
+						className={classes.backGr}
+					>
+						<img
+							className={classes.imgg}
+							src={fetched?.mainPhoto}
+							alt=""
+						/>
+					</div>
+				</div>
+				<p className={classes.title}>{fetched?.title}</p>
+				<p
+					dangerouslySetInnerHTML={{ __html: codeMy }}
+					className={classes.desc}
+				></p>
+				<h1>{specifications?.name}</h1>
+			</div>
+		);
+	});
+
 	return (
 		<div>
+			<div className={classes.fl}>
+				<div>
+					<FontAwesomeIcon
+						icon={faArrowLeft}
+						className={classes.icon}
+						onClick={navigateHandler}
+					/>
+				</div>
+				<div className={classes.icons}>
+					<FontAwesomeIcon icon={faGlobe} />
+					<span>En</span>
+				</div>
+			</div>
+
 			{allProducts}
-			<button onClick={navigateHandler}>Navigate</button>
 		</div>
 	);
 };
